c3bbc1d97848d2aa53636cf63843890ed5a4d016
The Blxck Cicada <68484817+TheBlxckCicada@users.noreply.github.com>
2024-04-07 12:59:48 +0200

    Update ADSecurityLab.ps1

--- ADSecurityLab.ps1
+++ ADSecurityLab.ps1
@@ -12,7 +12,7 @@ function Create-AD {
 
 <#
 .SYNOPSIS
-    Installs Active Directory Services and sets up a domain called adinsecurelab.local
+    Installs Active Directory Services and sets up a domain called umbrella.corp
  
 .EXAMPLE
      Create-AD
@@ -33,15 +33,15 @@ Install-WindowsFeature –Name AD-Domain-Services -IncludeManagementTools
 Add-windowsfeature RSAT-ADDS
 Import-Module ADDSDeployment
 
-$netbiosName = "ADINSECURELAB"
+$netbiosName = "UMBRELLA"
 $secpw = ConvertTo-SecureString "Password123" -AsPlainText -Force
-$domainname = "adinsecurelab.local"
+$domainname = "umbrella.corp"
 
 Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012" -DomainName $domainname -DomainNetbiosName $netbiosName -ForestMode "Win2012" -InstallDns -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false  -SafeModeAdministratorPassword (Convertto-SecureString -AsPlainText "Vagrant90" -Force) -SysvolPath "C:\Windows\SYSVOL" -Force:$tru
 
 
 Write-Output "[+] Installing Windows AD Domain Services and setting up domain"
-Write-Output "adinsecurelab.local created"
+Write-Output "umbrella.corp created"
 Write-Output "Please reboot, if not already your Domain Controller and login as the domain administrator and run setupscript2.ps1 as that use"
 
 }
@@ -71,32 +71,32 @@ Write-Output "[+] Creating Users and setting up Active Directory misconfiguratio
 Import-Module ActiveDirectory
 
 
-New-ADOrganizationalUnit -Name "Managers" -Path "DC=adinsecurelab,DC=local"
-NEW-ADGroup –name "Managers" –groupscope Global –path "OU=Managers,DC=adinsecurelab,DC=local"
-New-ADUser -Name "Jack Robinson" -GivenName "Jack" -Surname "Robinson"  -Description "Samrobinson123" -SamAccountName "Jack.Robinson" -UserPrincipalName "J.Robinson@adinsecure.lab" -Path "OU=Managers,DC=adinsecurelab,DC=local" -AccountPassword (Convertto-SecureString -AsPlainText "Samrobinson123" -Force) -Enabled $true
-New-ADUser -Name "Joe Standing" -GivenName "Joe" -Surname "Standing"  -Description "Execute Vice Preseident of Strategic Innovation, Holistic Integration and Corporate Development" -SamAccountName "Joe.Standing" -UserPrincipalName "J.Standing@adinsecure.lab" -Path "OU=Managers,DC=adinsecurelab,DC=local" -AccountPassword (Convertto-SecureString -AsPlainText "Executive12399$" -Force) -Enabled $true
+New-ADOrganizationalUnit -Name "Managers" -Path "DC=umbrella,DC=corp"
+NEW-ADGroup –name "Managers" –groupscope Global –path "OU=Managers,DC=umbrella,DC=corp"
+New-ADUser -Name "Jack Robinson" -GivenName "Jack" -Surname "Robinson"  -Description "Samrobinson123" -SamAccountName "Jack.Robinson" -UserPrincipalName "J.Robinson@adinsecure.lab" -Path "OU=Managers,DC=umbrella,DC=corp" -AccountPassword (Convertto-SecureString -AsPlainText "Samrobinson123" -Force) -Enabled $true
+New-ADUser -Name "Joe Standing" -GivenName "Joe" -Surname "Standing"  -Description "Execute Vice Preseident of Strategic Innovation, Holistic Integration and Corporate Development" -SamAccountName "Joe.Standing" -UserPrincipalName "J.Standing@adinsecure.lab" -Path "OU=Managers,DC=umbrella,DC=corp" -AccountPassword (Convertto-SecureString -AsPlainText "Executive12399$" -Force) -Enabled $true
 Add-ADGroupMember -Identity Managers -Members Jack.Robinson,Joe.Standing
 net group "Domain Admins" Joe.Standing /ADD /DOMAIN
 
-New-ADOrganizationalUnit -Name "HR" -Path "DC=adinsecurelab,DC=local"
-NEW-ADGroup –name "HR" –groupscope Global –path "OU=HR,DC=adinsecurelab,DC=local"
-New-ADUser -Name "Katie Haggerty" -GivenName "Katie" -Surname "Haggerty"  -Description "HR Staff1" -SamAccountName "Katie.Haggerty" -UserPrincipalName "K.Haggerty@adinsecure.lab" -Path "OU=HR,DC=adinsecurelab,DC=local" -AccountPassword (Convertto-SecureString -AsPlainText "Str0ngPaSS67" -Force) -Enabled $true
-New-ADUser -Name "Olivia Weidman" -GivenName "Olivia" -Surname "Weidman"  -Description "HR Staff1" -SamAccountName "Olivia.Weidman" -UserPrincipalName "O.Weidman@adinsecure.lab" -Path "OU=HR,DC=adinsecurelab,DC=local" -AccountPassword (Convertto-SecureString -AsPlainText "St4SDFxSS11434DF" -Force) -Enabled $true
+New-ADOrganizationalUnit -Name "HR" -Path "DC=umbrella,DC=corp"
+NEW-ADGroup –name "HR" –groupscope Global –path "OU=HR,DC=umbrella,DC=corp"
+New-ADUser -Name "Katie Haggerty" -GivenName "Katie" -Surname "Haggerty"  -Description "HR Staff1" -SamAccountName "Katie.Haggerty" -UserPrincipalName "K.Haggerty@adinsecure.lab" -Path "OU=HR,DC=umbrella,DC=corp" -AccountPassword (Convertto-SecureString -AsPlainText "Str0ngPaSS67" -Force) -Enabled $true
+New-ADUser -Name "Olivia Weidman" -GivenName "Olivia" -Surname "Weidman"  -Description "HR Staff1" -SamAccountName "Olivia.Weidman" -UserPrincipalName "O.Weidman@adinsecure.lab" -Path "OU=HR,DC=umbrella,DC=corp" -AccountPassword (Convertto-SecureString -AsPlainText "St4SDFxSS11434DF" -Force) -Enabled $true
 Add-ADGroupMember -Identity HR -Members Olivia.Weidman,Katie.Haggerty
 
 
 
-New-ADOrganizationalUnit -Name "Dev" -Path "DC=adinsecurelab,DC=local"
-NEW-ADGroup –name "Dev" –groupscope Global –path "OU=Dev,DC=adinsecurelab,DC=local"
-New-ADUser -Name "Rob Pratt" -GivenName "Robert" -Surname "Pratt"  -Description ".NET Contractor" -SamAccountName "Robert.Pratt" -UserPrincipalName "R.Pratt@adinsecure.lab" -Path "OU=Dev,DC=adinsecurelab,DC=local" -AccountPassword (Convertto-SecureString -AsPlainText "Password123" -Force) -Enabled $true
-New-ADUser -Name "Pritesh Choudwory" -GivenName "Pritesh" -Surname "Choudwory"  -Description ".NET Contractor 2" -SamAccountName "Pritest.Choudwory" -UserPrincipalName "P.Choudwory@adinsecure.lab" -Path "OU=Dev,DC=adinsecurelab,DC=local" -AccountPassword (Convertto-SecureString -AsPlainText "PaSS12345678" -Force) -Enabled $true
+New-ADOrganizationalUnit -Name "Dev" -Path "DC=umbrella,DC=corp"
+NEW-ADGroup –name "Dev" –groupscope Global –path "OU=Dev,DC=umbrella,DC=corp"
+New-ADUser -Name "Rob Pratt" -GivenName "Robert" -Surname "Pratt"  -Description ".NET Contractor" -SamAccountName "Robert.Pratt" -UserPrincipalName "R.Pratt@adinsecure.lab" -Path "OU=Dev,DC=umbrella,DC=corp" -AccountPassword (Convertto-SecureString -AsPlainText "Password123" -Force) -Enabled $true
+New-ADUser -Name "Pritesh Choudwory" -GivenName "Pritesh" -Surname "Choudwory"  -Description ".NET Contractor 2" -SamAccountName "Pritest.Choudwory" -UserPrincipalName "P.Choudwory@adinsecure.lab" -Path "OU=Dev,DC=umbrella,DC=corp" -AccountPassword (Convertto-SecureString -AsPlainText "PaSS12345678" -Force) -Enabled $true
 Add-ADGroupMember -Identity Dev -Members Pritest.Choudwory,Robert.Pratt
 
 
 
 Write-Output "[+] Creating Kerberoasting setup"
-net localgroup administrators ADINSECURELAB\Robert.Pratt /add
-setspn -s http/adinsecurelab.local:80 Robert.Pratt
+net localgroup administrators UMBRELLA\Robert.Pratt /add
+setspn -s http/umbrella.corp:80 Robert.Pratt
 
 Write-Output "[+] Enabling WinRM, if not already enabled and misconfiguring settings"
 winrm quickconfig -transport:http -quiet -force
@@ -131,21 +131,21 @@ $ace = New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
 $acl.AddAccessRule($ace)
 Set-ACL "AD:\$ou" -AclObject $acl
 
-$Sid = (Get-ADObject -Identity "CN=Robert Pratt,OU=Dev,DC=adinsecurelab,DC=local").ObjectGUID
+$Sid = (Get-ADObject -Identity "CN=Robert Pratt,OU=Dev,DC=umbrella,DC=corp").ObjectGUID
 $NewAccessRule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule($Sid, "WriteDacl", "Allow", "Domain Admins")
 $Acl.AddAccessRule($NewAccessRule)
 Set-ACL "AD:\$ou" -AclObject $acl
 
 
 # Test with : Get-ObjectAcl -SamAccountName "Managers" -ResolveGUIDs | ? {$_.ActiveDirectoryRights -eq "WriteOwner"}
-# Get-ACL "AD:\OU=Managers,DC=adinsecurelab,DC=local").Access |
-# Where-Object {$_.IdentityReference -eq "adinsecurelab\Katie.Haggerty"}  
+# Get-ACL "AD:\OU=Managers,DC=umbrella,DC=corp").Access |
+# Where-Object {$_.IdentityReference -eq "umbrella\Katie.Haggerty"}  
 
 Write-Output "[+] Creating Passwords in SYSVOL"
 
-new-gpo -name MarketingGPO | new-gplink -target "OU=HR,DC=adinsecurelab,DC=local" | set-gppermissions -permissionlevel gpoedit -targetname "HR" -targettype group
+new-gpo -name MarketingGPO | new-gplink -target "OU=HR,DC=umbrella,DC=corp" | set-gppermissions -permissionlevel gpoedit -targetname "HR" -targettype group
 
-Write-Output "net user /add Olivia.Weidman St4SDFxSS11434DF" > "C:\Windows\SYSVOL\sysvol\adinsecurelab.local\scripts\create_backupuser.ps1"
+Write-Output "net user /add Olivia.Weidman St4SDFxSS11434DF" > "C:\Windows\SYSVOL\sysvol\umbrella.corp\scripts\create_backupuser.ps1"
 
 gpupdate
 
@@ -155,8 +155,8 @@ $Groups_xml = @"
 <Groups clsid="{3125E937-EB16-4b4c-9934-544FC6D24D26}"><User clsid="{DF5F1855-51E5-4d24-8B1A-D9BDE98BA1D1}" name="Katie.H_Backup" image="2" changed="2019-08-27 21:07:40" uid="{0BF14BE4-4AAD-44DD-933C-6DA54774FA2E}"><Properties action="U" newName="" fullName="Katie" description="Katie Backup Admin Account For Temp Use" cpassword="JCzwVAEdHyQeEAHXGNhtuSu9nOdiLr9x3kzmXGWd9xo" changeLogon="0" noChange="0" neverExpires="0" acctDisabled="0" userName="Katie.Hagggerty"/></User>
 </Groups>
 "@
-$groupspath = "C:\Windows\SYSVOL\sysvol\adinsecurelab.local\Policies\"
-$filepath = Get-ChildItem "C:\Windows\SYSVOL\sysvol\adinsecurelab.local\Policies\"
+$groupspath = "C:\Windows\SYSVOL\sysvol\umbrella.corp\Policies\"
+$filepath = Get-ChildItem "C:\Windows\SYSVOL\sysvol\umbrella.corp\Policies\"
 $sysvolpath = $filepath.BaseName[1]
 $filepath = "$groupspath$sysvolpath\"
 mkdir "$filepath\MACHINE\Preferences\Groups\"
@@ -168,7 +168,7 @@ Set-ItemProperty -Path HKLM:\SYSTEM\Setup -Name Pritesh -Value "PaSS12345678:UGF
 
 echo "[+] Enabling AlwaysInstallElevated registry key"
 
-$objUser = New-Object System.Security.Principal.NTAccount("adinsecurelab.local", "Joe.Standing") 
+$objUser = New-Object System.Security.Principal.NTAccount("umbrella.corp", "Joe.Standing") 
 $strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier]) 
 $userSID = $strSID.Value
 New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\Installer 
